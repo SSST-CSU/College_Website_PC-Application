bbb49be3ec1ce587b9d2396eb11849f7ecb84424
pzf0000 <pzf0000@foxmail.com>
2018-03-26 02:19:44 +0800

    初始化

--- PC-Application/MainWindow.xaml.cs
+++ PC-Application/MainWindow.xaml.cs
@@ -20,9 +20,15 @@ namespace PC_Application
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 屏幕宽度与高度
+        public static new double ScreenWidth = SystemParameters.PrimaryScreenWidth;
+        public static new double ScreenHeight = SystemParameters.PrimaryScreenHeight;
+
         public MainWindow()
         {
             InitializeComponent();
+            Width = ScreenWidth * 0.5;
+            Height = ScreenHeight * 0.5;
         }
     }
 }
